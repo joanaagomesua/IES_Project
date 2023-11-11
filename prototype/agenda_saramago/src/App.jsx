e7df1f9df8096dc7ced842db6be2f64e7e5e1c4f
lilikas11 <lilianapcribeiro@gmail.com>
2023-11-11 17:00:57 +0000

    route

--- prototype/agenda_saramago/src/App.jsx
+++ prototype/agenda_saramago/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
-import event_page from './pages/event_page';
-import navbar from './components/navbar';
+import Navbar from './components/navbar.jsx';
 
 function App() {
   return (
     <BrowserRouter >
-      <navbar className="bg-[#19A7CE]" />
+      <Navbar className="bg-[#19A7CE]" />
       <Routes>
       <Route path="/" element={<event_page />} />
       </Routes>
