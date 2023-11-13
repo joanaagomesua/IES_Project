98ed16c119e62952c42a0ca892ed112c3df67198
Lia Cardoso <lia.cardoso@ua.pt>
2023-11-13 16:23:35 +0000

    event page on the making

--- prototype/agenda_saramago/src/pages/event_page.jsx
+++ prototype/agenda_saramago/src/pages/event_page.jsx
@@ -3,6 +3,8 @@ import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import CarouselWithContent from '../components/carousel.jsx';
 import Map from '../components/map.jsx'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChair, faChild, faPerson, faPersonCane } from '@fortawesome/free-solid-svg-icons';
 
 function event_page(){
     return (
@@ -11,13 +13,20 @@ function event_page(){
                 <p className='font-poppins font-bold text-4xl'>The Magical Fairy Kingdom</p>
                 <p className='font-poppins text-3xl'>By: Tiny Kids</p>
             </div>
-            <div className='flex bg-green-200 space-x-10'>
+            <div className=' flex bg-green-200 space-x-10'>
                 <div className='flex-initial w-1/4'>
                     <img src="./src/assets/images/poster_test.png"></img>
                 </div>
                 <div className='w-1/2 flex flex-col'>
-                    <div className='font-poppins font-bold mb-4'>
-                        LOTAÇÃO + PREÇOS
+                    <div className='flex font-poppins font-bold mb-4'>
+                        <div className='w-1/2 flex flex-row items-center'>
+                            <FontAwesomeIcon icon={faChair}  className="mr-2" />
+                            <div className="rounded-full w-full bg-neutral-200 dark:bg-neutral-600">
+                                <div className="bg-primary p-2 text-center text-xs font-medium leading-none text-primary-100" style={{ width: '25%' }}>
+                                25%
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className='font-poppins w-full mb-4'>
                         <p>The play begins with a beautiful and captivating opening scene that sets the tone for the rest of the performance. The stage is transformed into a magical forest, complete with towering trees, colorful flowers, and twinkling fairy lights. The audience is transported to a world of wonder and enchantment, where anything is possible.
@@ -35,7 +44,42 @@ function event_page(){
                     </div>
                     <div className='font-poppins'>
                         <h2 className='font-bold mb-2'>Tags:</h2>
-                        <div class="box-border h-20 w-full p-4 border-2 border-black"> 
+                        <div class="box-border h-20 w-full p-4 border-2 border-black border-opacity-10">
+                            <div className='flex space-x-2'>
+                                <div className="rounded-full bg-slate-200 bg-opacity-100 p-2">
+                                    <p className="text-black">#theatre</p>
+                                </div>
+                                <div className="rounded-full bg-slate-200 bg-opacity-100 p-2">
+                                    <p className="text-black">#kids</p>
+                                </div>
+                                <div className="rounded-full bg-slate-200 bg-opacity-100 p-2">
+                                    <p className="text-black">#happy</p>
+                                </div>
+                                <div className="rounded-full bg-slate-200 bg-opacity-100 p-2">
+                                    <p className="text-black">#fantasy</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='font-poppins'>
+                    <h2 className='font-bold text-2xl mb-4'>Preços</h2>
+                    <div className='flex flex-col'>
+                        <div className="flex items-center">
+                            <FontAwesomeIcon icon={faChild}  className="mr-4 mb-2" />
+                            <h3 className='mr-2'>Children:</h3>
+                            <br/>
+                            <p>2€</p>
+                        </div>
+                        <div className="flex items-center">
+                            <FontAwesomeIcon icon={faPerson}  className="mr-4 mb-2" />
+                            <h3 className='mr-2'>Adults:</h3>
+                            <p>6€</p>
+                        </div>
+                        <div className="flex items-center">
+                            <FontAwesomeIcon icon={faPersonCane}  className="mr-4 mb-2" />
+                            <h3 className='mr-2'>Seniores:</h3>
+                            <p>3€</p>
                         </div>
                     </div>
                 </div>
