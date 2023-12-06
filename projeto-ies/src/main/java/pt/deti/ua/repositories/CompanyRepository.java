d699b640e6dc83117639d5fc4d0aefda5f4972f9
Lia Cardoso <lia.cardoso@ua.pt>
2023-12-06 16:38:17 +0000

    company

--- projeto-ies/src/main/java/pt/deti/ua/repositories/CompanyRepository.java
+++ projeto-ies/src/main/java/pt/deti/ua/repositories/CompanyRepository.java
@@ -1,9 +1,12 @@
 import org.springframework.stereotype.Repository;
 
 import pt.deti.ua.models.Company;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public interface CompanyRepository extends MongoRepository<Company, Long> {
+public interface CompanyRepository extends JpaRepository<Company, String> {
+    public Company findByName(String name);
     public List<Company> findByType(String type);
 
 }
