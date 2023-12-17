667dfcdbe9bc77e1972035ac3bfb15b995b268f5
lilikas11 <lilianapcribeiro@gmail.com>
2023-12-17 00:50:06 +0000

    merge mas bem feito

--- prototype/agenda_saramago/src/pages/event_page.jsx
+++ prototype/agenda_saramago/src/pages/event_page.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { Fragment } from 'react';
 import axios from "axios";
 import CarouselWithContent from '../components/carousel.jsx';
 import Map from '../components/map.jsx'
@@ -17,7 +16,6 @@ function event_page() {
         const mes = data.getMonth() + 1;
         const ano = data.getFullYear();
     
-        // Adicionando zeros à esquerda para garantir que o formato seja 'dd/mm/yyyy'
         const diaFormatado = dia < 10 ? `0${dia}` : dia;
         const mesFormatado = mes < 10 ? `0${mes}` : mes;
     
@@ -38,7 +36,6 @@ function event_page() {
       }, [id]);
 
       useEffect(() => {
-        // Redirect to "/event_page/1" if the data is loaded
         if (eventData) {
         }
       }, [eventData, id, history]);
@@ -58,7 +55,7 @@ function event_page() {
                     <div className=' flex bg-green-200 space-x-10'>
                             <div className='flex-initial w-1/4'>
                                 <div className='relative'>
-                                    <img src={eventData.poster} alt="Event Poster"></img> {/*Image-> POSTER */}
+                                    <img src={eventData.poster} alt="Event Poster"></img>{/*Image-> POSTER */}
                                     <div className="absolute top-0 right-0 p-4 flex items-center justify-center">
                                         <button className="w-10 h-10 rounded-full bg-slate-300 hover:bg-slate-500 text-white">
                                             <FontAwesomeIcon icon={faPlus}/>
@@ -125,7 +122,7 @@ function event_page() {
                                         <h2 className='font-bold text-2xl mt-4 mb-2'>Schedule</h2>
                                         <p>{eventData.schedule}</p>
                                         <h2 className='font-bold text-2xl mt-4 mb-2'>Duration</h2>
-                                        <p>Not Defined</p>
+                                        <p>{eventData.duration} minutos</p>
                                 </div>
                             </div>
                         </div>
@@ -156,44 +153,7 @@ function event_page() {
                                 <Map  width="560px" height="390px"/>
                             </div>
                         </div>
-                    </div>
-                    <div className='mt-4 mb-4'>
-                    <h2 className='font-bold text-2xl mt-4 mb-2'>Date</h2>
-                        <p>20 de novembro de 2023</p>
-                        <h2 className='font-bold text-2xl mt-4 mb-2'>Schedule</h2>
-                        <p>15:30</p>
-                        <h2 className='font-bold text-2xl mt-4 mb-2'>Duration</h2>
-                        <p>50 minutos</p>
-
-                    </div>
-                </div>
-            </div>
-            <div className='flex h-auto bg-yellow-200 flex-col-reverse md:flex-row sm:flex-col'>
-            <div className='flex flex-col w-full md:w-2/3'>
-            <h1 className='font-poppins font-bold text-3xl mb-4'>Similar Events</h1>
-            <div className='flex items-start space-x-10'>
-              <div>
-                <h3 className='font-poppins text-2xl mb-2'>For kids...</h3>
-                <CarouselWithContent/>
-              </div>
-              <div className='border-l border-gray-500 pl-4'>
-                <div className='ml-6'>
-                    <h3 className='font-poppins text-2xl mb-2'>For theatre lovers...</h3>
-                    <CarouselWithContent />
-                </div>
-              </div>
-              <div className='border-l border-gray-500 pl-4'>
-                <div className='ml-6'>
-                <h3 className='font-poppins text-2xl mb-2'>Fantasy...</h3>
-                <CarouselWithContent/>
-                </div>
-              </div>
-                </div>
-                </div>
-                <div className='w-full md:w-1/3 flex flex-col order-3'>
-                    
-                </div>
-            </div>
+        )
         </div>
     );
     }
